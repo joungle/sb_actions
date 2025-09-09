684b10809f65cebad54d7cdbb80a3c6c406c5db1
jan <janlenartz@gmail.com>
2025-09-09 22:43:02 +0200

    test

--- raw_gitlab.py
+++ raw_gitlab.py
@@ -1,14 +1,24 @@
+import os
 from seleniumbase import SB
 
-with SB(uc=True, test=True, locale_code="en") as sb:
-    url = "https://gitlab.com/users/sign_in"
-    sb.activate_cdp_mode(url)
-    sb.sleep(5)
+USER = os.environ["USER"]
+PW = os.environ["PW"]
+
+with SB(uc=True, test=True, locale_code="de") as sb:
+    sb.activate_cdp_mode("https://login.rescuetrack.com/login/login.html")
+    sb.sleep(1)
+    sb.driver.cdp.click("input#login-username")
+    sb.driver.cdp.press_keys("input#login-username", USER)
+    sb.driver.cdp.click("input#login-password")
+    sb.driver.cdp.press_keys("input#login-password", PW)
+    sb.sleep(2)
+    sb.driver.cdp.click("login-button")
+    sb.sleep(2)
+    sb.driver.cdp.open("https://apps.rescuetrack.com/rmp/")
+    sb.sleep(3)
+    sb.driver.cdp.click(f'div.button.assignButton[data-target="#assignTransportModal71154072"]')
+    sb.sleep(1)
     sb.uc_gui_click_captcha()
-    sb.assert_text("Username", '[for="user_login"]', timeout=3)
-    sb.assert_element('label[for="user_login"]')
-    sb.highlight('button:contains("Sign in")')
-    sb.highlight('h1:contains("GitLab.com")')
-    sb.post_message("SeleniumBase wasn't detected", duration=4)
+    sb.sleep(4)
     sb.save_screenshot_to_logs()
     print("Success! Website did not detect SeleniumBase!")
