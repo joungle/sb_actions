fd2ed648dc6dff5905dc1e30863b869926a063b3
jan <janlenartz@gmail.com>
2025-09-09 21:43:23 +0200

    Five

--- raw_gitlab.py
+++ raw_gitlab.py
@@ -11,4 +11,3 @@ with SB(uc=True, test=True, locale_code="en") as sb:
     sb.highlight('h1:contains("GitLab.com")')
     sb.post_message("SeleniumBase wasn't detected", duration=4)
     print("Success! Website did not detect SeleniumBase!")
-    
