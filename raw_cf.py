9a9762b18f2dbff7925ca5a8736823462f7de8e4
jan <janlenartz@gmail.com>
2025-09-16 19:21:05 +0200

    Testing 11

--- raw_cf.py
+++ raw_cf.py
@@ -8,3 +8,13 @@ with SB(uc=True, test=True, locale="en", guest=True) as sb:
     sb.uc_gui_click_captcha()
     sb.sleep(4)
     sb.save_screenshot_to_logs()
+    cf_cookie = None
+    all_cookies = sb.get_all_cookies()
+    for cookie in all_cookies:
+        if cookie.name == 'cf_clearance':
+            cf_cookie = cookie
+            break
+    if cf_cookie:
+        print("cf_clearance cookie: %s" % cf_cookie.value)
+    else:
+        print("Didn't find the cf_clearance cookie!")
