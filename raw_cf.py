e7ddf9616ff81c4d5bbd8a6a63b9b90dede9fd22
jan <janlenartz@gmail.com>
2025-09-16 18:58:08 +0200

    Testing 8

--- raw_cf.py
+++ raw_cf.py
@@ -5,6 +5,7 @@ with SB(uc=True, test=True, locale="en", guest=True) as sb:
     sb.activate_cdp_mode(url)
     sb.sleep(4)
     sb.save_screenshot_to_logs()
-    sb.uc_gui_click_captcha()  # PyAutoGUI click. (Linux needs it)
+    sb.cdp.gui_click_element(".c_hh div div")
+    # sb.uc_gui_click_captcha()
     sb.sleep(4)
     sb.save_screenshot_to_logs()
