1b5e900138d265df3a1d7c38896846dcef18a581
jan <janlenartz@gmail.com>
2025-09-16 18:54:25 +0200

    Testing 7

--- raw_cf.py
+++ raw_cf.py
@@ -3,7 +3,8 @@ from seleniumbase import SB
 with SB(uc=True, test=True, locale="en", guest=True) as sb:
     url = "https://www.cloudflare.com/login"
     sb.activate_cdp_mode(url)
-    sb.sleep(3)
+    sb.sleep(4)
+    sb.save_screenshot_to_logs()
     sb.uc_gui_click_captcha()  # PyAutoGUI click. (Linux needs it)
-    sb.sleep(2)
+    sb.sleep(4)
     sb.save_screenshot_to_logs()
